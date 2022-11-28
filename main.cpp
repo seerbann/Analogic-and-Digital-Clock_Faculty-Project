b28761ef6bfc7b240359d1dbe8d36c3269431e95
seerbann <serbypwp@yahoo.com>
2022-11-28 19:45:40 +0200

    transToWinBgi

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <graphics.h>
 #include <ctime>
 #include <windows.h>
 using namespace std;
 
-struct {
+struct
+{
     int seconds;
     int minutes;
     int hours;
-}ceas;
+} ceas;
 
 void getTime()
 {
@@ -18,18 +20,16 @@ void getTime()
     ceas.hours = (timePtr->tm_hour);
 }
 
-int main()
+displayTime()
 {
-
-    getTime();
     while (true)
     {
-        system("cls");
+        cleardevice();
+        bgiout<<"Ora este ";
+        outstreamxy(50,50);
 
-        cout << "Timpul digital este:";
-        // This output the message "The digital time is :"
-
-        cout << "      " << ceas.hours << " : " << ceas.minutes << " : " << ceas.seconds << " " << endl;
+        bgiout << "      " << ceas.hours << " : " << ceas.minutes << " : " << ceas.seconds << " " << endl; ;
+        outstreamxy(100, 100);
         //increment sec min and hours
         ceas.seconds++;
         if (ceas.seconds >= 60)
@@ -49,8 +49,22 @@ int main()
             ceas.hours= 00;
         }
 
-        Sleep(1000); // opreste codul timp de 1000 ms(o secunda)
+        delay(1000); // opreste codul timp de 1000 ms(o secunda)
     }
+}
 
+int main()
+{
+
+
+
+    initwindow(800,600);
+
+    getTime();
+    displayTime();
+
+    getch();
+    closegraph();
+    return 0;
     return 0;
 }
