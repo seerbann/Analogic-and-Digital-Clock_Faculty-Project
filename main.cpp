59242503f365b0c51b0f511e9c84fbb565fc9ea1
seerbann <100155210+seerbann@users.noreply.github.com>
2022-12-03 15:30:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 #define MAX 20
 #define FUNDAL CYAN
+
 struct punct
 {
     int x, y;
@@ -15,52 +16,6 @@ struct dreptunghi
 {
     punct SS, DJ;
 };
-
-struct
-{
-    int secunde;
-    int minute;
-    int ore;
-} ceas;
-
-void iaTimpulLocal()
-{
-    time_t t = time(NULL);
-    tm *timePtr = localtime(&t); // ia timpul local al calculatorului
-    ceas.secunde = (timePtr->tm_sec);
-    ceas.minute = (timePtr->tm_min);
-    ceas.ore = (timePtr->tm_hour);
-}
-
-void scrieTimpul()
-{
-
-       // cleardevice(); //sterge consola
-        //stilizare
-       // setcolor(9);
-       // settextstyle(3, HORIZ_DIR,7 );
-        //outstreamxy(50,50);
-
-        bgiout << "      " << ceas.ore << " : " << ceas.minute << " : " << ceas.secunde << " " << endl; ;
-        outstreamxy(getmaxx()/2-75, getmaxy()/2);
-        //incrementeaza sec min si ore
-        ceas.secunde++;
-        if (ceas.secunde >= 60)
-        {
-            ceas.secunde = 1;
-            ceas.minute++;
-        }
-        if (ceas.minute >= 60)
-        {
-            ceas.minute = 0;
-            ceas.ore++;
-        }
-        if (ceas.ore > 24)
-        {
-            ceas.ore= 00;
-        }
-        delay(1000); // opreste codul timp de 1000 ms(o secunda)
-}
 
 bool apartine(punct P, dreptunghi D)
 {
@@ -81,8 +36,8 @@ void deseneazaMeniul()
 {
     setcolor(WHITE);
     rectangle(0,150,getmaxx(),getmaxy());
-   // setcolor(LIGHTBLUE);
-  //  setfillstyle(SOLID_FILL,LIGHTCYAN);
+    setcolor(LIGHTBLUE);
+    setfillstyle(SOLID_FILL,LIGHTCYAN);
     int i;
     for (i=1; i<=nrButoane; i++)
     {
@@ -99,7 +54,7 @@ void deseneazaMeniul()
             strcpy(B[i].text,"Joc");
             break;
         case 3:
-            strcpy(B[i].text,"Coma. 3");
+            strcpy(B[i].text,"DIGITAL");
             break;
         case 4:
             strcpy(B[i].text,"Alta");
@@ -110,7 +65,7 @@ void deseneazaMeniul()
         }
         rectangle(B[i].D.SS.x, B[i].D.SS.y,B[i].D.DJ.x,B[i].D.DJ.y);
         bar(B[i].D.SS.x, B[i].D.SS.y+30, B[i].D.DJ.x, B[i].D.SS.y);
-      //  setbkcolor(LIGHTCYAN);
+        setbkcolor(LIGHTCYAN);
         outtextxy(B[i].D.SS.x+25,B[i].D.SS.y+10,B[i].text);
     }
 }
@@ -131,29 +86,89 @@ int butonAles()
     return 0;
 }
 
+struct
+{
+    int secunde;
+    int minute;
+    int ore;
+} ceas;
+
+void iaTimpulLocal()
+{
+    time_t t = time(NULL);
+    tm *timePtr = localtime(&t); // ia timpul local al calculatorului
+    ceas.secunde = (timePtr->tm_sec);
+    ceas.minute = (timePtr->tm_min);
+    ceas.ore = (timePtr->tm_hour);
+}
+
+void afisMeniuDigital()
+{
+    rectangle(10,10,40,30);
+    outtextxy(18,12,"<-");
+}
+
+
+void scrieTimpul()
+{
+    while (true)
+    {
+        setbkcolor(1);
+        cleardevice(); //sterge consola
+        //stilizare
+       // setcolor(9);
+        //settextstyle(3, HORIZ_DIR,7 );
+        //outstreamxy(50,50);
+        afisMeniuDigital();
+        bgiout << "      " << ceas.ore << " : " << ceas.minute << " : " << ceas.secunde << " " << endl; ;
+        outstreamxy(370, 270);
+        //incrementeaza sec min si ore
+        ceas.secunde++;
+        if (ceas.secunde >= 60)
+        {
+            ceas.secunde = 1;
+            ceas.minute++;
+        }
+        if (ceas.minute >= 60)
+        {
+            ceas.minute = 0;
+            ceas.ore++;
+        }
+        if (ceas.ore > 24)
+        {
+            ceas.ore= 00;
+        }
+        delay(1000); // opreste codul timp de 1000 ms(o secunda)
+    }
+}
+
 int main()
 {
-    initwindow(800,600);
+    initwindow(900,600);
+    deseneazaMeniul();
+    setactivepage(1);
     iaTimpulLocal();
     int comanda, butonul_apasat;
     do
-    {   scrieTimpul();
-        deseneazaMeniul();
+    {
+
         butonul_apasat=butonAles();
         if (butonul_apasat!=0)
-            {
+        {
+            comanda=butonul_apasat;
+            cout<<"Comanda "<<comanda<<endl;
 
-                comanda=butonul_apasat;
-                if(comanda==1)
-                    cleardevice();
-                if(comanda==2)
-                    ceas.ore=ceas.ore+2;
-                cout<<"Comanda "<<comanda<<endl;
+            if(comanda==3)
+            {
+                setactivepage(2);
+                setvisualpage(2);
+                scrieTimpul();
             }
+        }
 
     }
     while (comanda!=5);
-
+    // getch();
     closegraph();
     return 0;
 }
